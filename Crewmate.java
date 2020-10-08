e9d81f825459ad6005fbd4b2c1572855b4989b59
Hudinid <69361082+Hudinid@users.noreply.github.com>
2020-10-08 00:05:00 -0700

    Create Crewmate.java
    just some stuff i typed up in like 10 mins + googling

---
+++ Crewmate.java
@@ -0,0 +1,18 @@
+//Have a use check in the game manager cuz both imps and crewmates can do it
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class Crewmate{
+    private int tasks = 5;
+    private boolean death = false;
+    private boolean use = false;
+    Player player;
+
+    public Use(){
+        while(use == true){
+            player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 1, 5));
+        }
+    }
+}
