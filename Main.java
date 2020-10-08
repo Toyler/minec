78bdc2fcc9273119613cf61f91506c432c257a3b
bxrr <xu.z.brandon@gmail.com>
2020-10-08 08:47:19 -0700

    yea

--- Main.java
+++ Main.java
@@ -7,8 +7,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class GameManager extends JavaPlugin implements Listener {
-	
+import Java.util.ArrayList;
+
+public class GameRun extends JavaPlugin implements Listener {
+	public static int imposterOne = -1;
+	public static int imposterTwo = -1;
+
+
+
 	@Override
 	public void onEnable() {
 		
@@ -16,31 +22,65 @@ public class GameManager extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onDisable() {
-		
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender instanceof Player) {
 			String name = cmd.getName();
 			if (name.equalsIgnoreCase("amongus")) {
+
 				if (args.length == 0) {
 					return false;
 				}
+
 				String command = args[0];
+				try
+				{
+					boolean areTwoImposters = args[1];
+				}
+				catch(Exception e)
+				{
+					System.out.println(e + "/amongus (start) (has two imposters{true/false})");
+				}
+
 				Player player = (Player) sender;
 				if (command.equalsIgnoreCase("start")) {
 					Bukkit.broadcastMessage("Game has started!");
-					for (Player player : Bukkit.getOnlinePlayers()) {
-						int impostorOne = (int) (Math.random() * 10 + 1);
-						int imposterTwo = imposterOne;
-						while(imposterOne == imposterTwo)
-						{
-							int impostorTwo = (int) (Math.random() * 10 + 1);
-						}
-					}
+					generateImposters(areTwoImposters);
+					System.out.println("Number of players: " + getNumPlayers() + ", Imposters: " + imposterOne + ", " + imposterTwo); // debug line
 				}
 			}
 		}
 		return false;
+	}
+
+	
+
+	public int getNumPlayers() {
+		return getPlayerNames().size();
+	}
+
+	public ArrayList<String> getPlayerNames() {
+		ArrayList<String> playerList = new ArrayList<String>();
+		Player onlinePlayers = Bukkit.getOnlinePlayers();
+		for(Player player : onlinePlayers)
+		{
+			playerList.add(player.getDisplayName());
+		}
+		
+		return playerList;
+	}
+
+	public void generateImposters(boolean hasTwoImposters)
+	{
+		imposterOne = (int) (Math.random() * getNumPlayers() + 1);
+		if(hasTwoImposters)
+		{
+			imposterTwo = imposterOne;
+			while(imposterOne == imposterTwo)
+			{
+				int impostorTwo = (int) (Math.random() * getNumPlayers() + 1);
+			}
+		}
 	}
 }
